e3c9c6506460bf36624b816c3f2e8249843dcce9
ItzTheDodo <57725407+ItzTheDodo@users.noreply.github.com>
2020-07-16 18:04:34 +0100

    Update Conn.py

--- Conn.py
+++ Conn.py
@@ -1,7 +1,7 @@
 if __name__ == "__main__":
     exit(-1)
 
-main_api_code = "9bae98c5-a05c-4650-95dc-f6b2549fd7d0"
+main_api_code = "<api-code>"
 
 Important_URLS = [
     "https://api.hypixel.net/skyblock/bazaar/product?key=" + main_api_code + "&productId=",
